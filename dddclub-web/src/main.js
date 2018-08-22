8312170cb4572ec38506d96d79ffd45372a2f17d
s1mple <798342418@qq.com>
2018-08-22 14:57:34 +0800

    #2 [s1mple] - 重新梳理前端脚手架项目结构

--- dddclub-web/src/main.js
+++ dddclub-web/src/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import router from './router/index'
+import store from './store/store'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App),
-}).$mount('#app');
+  render: h => h(App)
+}).$mount('#app')
