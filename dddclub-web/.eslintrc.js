fa2e25121c94c4bab340d2c6c932f086d763e426
s1mple <798342418@qq.com>
2018-08-22 15:15:41 +0800

    #2 [s1mple] - 修改 ESLint 规则

--- dddclub-web/.eslintrc.js
+++ dddclub-web/.eslintrc.js
@@ -1,17 +1,30 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
   extends: [
     'plugin:vue/essential',
-    '@vue/airbnb',
+    '@vue/airbnb'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // allow console during development
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // allow unused variables during development
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    // disallow trailing commas
+    'comma-dangle': ['error', 'never'],
+    // disallow trailing semi
+    semi: ['error', 'never'],
+    // allow the unary operators ++ and --
+    'no-plusplus': 'off',
+    'object-curly-newline': 'off',
+    // enforce a maximum line length
+    'max-len': [0, 150]
   },
   parserOptions: {
-    parser: 'babel-eslint',
-  },
-};
+    parser: 'babel-eslint'
+  }
+}
